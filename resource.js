38581da24bbde4d83987bc6949ee695a1cf0eb9e
kurosaki <kemco.kurosaki@gmail.com>
2026-05-22 12:56:28 +0900

    Auto-commit: Claude Code changes

--- resource.js
+++ resource.js
@@ -513,7 +513,7 @@ function renderOwnerDetailTimeline(ownerName) {
                     <div class="resource-cell-bar ${colorClass}"
                          style="position: absolute; top: 4px; height: 22px; left: ${left}px; width: ${barWidth}px; z-index: 10;"
                          title="${t.text} (${t.project_number})">
-                         <span class="resource-bar-text" style="color:${textColor}; font-size:11px; font-weight:bold;">${t.project_number || ""} ${t.text}</span>
+                         <span class="resource-bar-text" style="color:${textColor}; font-size:11px; font-weight:bold;">${[t.project_number, t.machine, t.unit, t.text].filter(v=>v).join(' ')}</span>
                     </div>` : ''}
                 </div>
             </div>
