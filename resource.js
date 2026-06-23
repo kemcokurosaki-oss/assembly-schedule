ca4b919c1db7d5e19dd4eea52b3d6bcc5b8469e0
kurosaki <kemco.kurosaki@gmail.com>
2026-06-23 09:34:31 +0900

    Auto-commit: Claude Code changes

--- resource.js
+++ resource.js
@@ -547,6 +547,8 @@ function renderOwnerDetailTimeline(ownerName) {
     _updateDetailToolbar(actualGridWidth);
     renderResourceCalendarHeader();
     syncResourceScroll();
+    initAssemblyResourceBarDragAndResize(container);
+    initAssemblyResourceBarOwnerPopup(container);
 }
 
 const _DOW_NAMES = ['日', '月', '火', '水', '木', '金', '土'];
